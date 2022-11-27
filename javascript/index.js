b1b5cd7d5a4835f2933c9a4a3ce29f22a3c66e38
Durgesh Mehar <108683573+durgeshmehar@users.noreply.github.com>
2022-11-27 23:53:08 +0530

    Update index.js

--- javascript/index.js
+++ javascript/index.js
@@ -1,18 +1,12 @@
+   function sunImage() {
+      $(".navigation ul li#light i").toggle();
 
-        $(".navigation ul li i").click(function()
+   };
+   $(".navigation ul li i#sun").hide();
 
-        {
-           $(".navigation").after("<p class='firstHr'>");
-           $(".footer").prepend("<p class='firstHr'>");
-           $(".upper-cloud ,.bottom-cloud").css("opacity","0.2");
-           $(".navigation ul li a").css("color","rgb(185, 147, 220)");
-          
-       
-           $(".firstHr").css({"border":"1px solid rgba(100,100,100,0.2)" ,"border-top":"none"});
-           $("*").css("background-color","rgb(40,39,50)");
-           $("p ,span").css("color" ,"white");
-       
-         }) ;
+$(".navigation ul li#light").click(function () {
 
-
-
+     $(".navigation ul li a i.icon-color").animate({ left: "2.5vw" }, 400);
+     $("body").toggleClass("dark-mode");
+     setTimeout(sunImage, 300);
+});
