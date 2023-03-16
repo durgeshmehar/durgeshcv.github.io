c1cbc573ebfe5bbceffdd3ddabf1b70f4d21792e
Durgesh Mehar <108683573+durgeshmehar@users.noreply.github.com>
2023-03-16 15:29:37 +0530

    Animation of view add

---
+++ javascript/move.js
@@ -0,0 +1,24 @@
+
+const observer = new IntersectionObserver((entries)=> {
+    entries.forEach((entry) => {
+        console.log(entry)
+        if( entry.isIntersecting){
+            entry.target.classList.add('show1');
+            entry.target.classList.add('show2');
+            entry.target.classList.add('show3');
+        }
+        else{
+            entry.target.classList.remove('show1');
+            entry.target.classList.remove('show2');
+            entry.target.classList.remove('show3');
+        }
+    });
+});
+
+const hiddenElements = document.querySelectorAll('.hidden1');
+const hiddenElement = document.querySelectorAll('.hidden2');
+const hiddenelement = document.querySelectorAll('.hidden3');
+
+hiddenElements.forEach((el)=> observer.observe(el));
+hiddenElement.forEach((el)=> observer.observe(el));
+hiddenelement.forEach((el)=> observer.observe(el));
