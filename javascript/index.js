3b7df5c7e9dc2e9cddfcd16a5ee1285dfa85f1d6
Durgesh Mehar <108683573+durgeshmehar@users.noreply.github.com>
2023-09-23 23:54:50 +0530

    Navbar height adjust fixed

--- javascript/index.js
+++ javascript/index.js
@@ -121,3 +121,20 @@ var typed = new Typed(".auto-type", {
 	backSpeed : 40,
 	loop : true
 });
+
+
+//6) Moving Animation Feature
+
+const observer = new IntersectionObserver((entries) => {
+	entries.forEach((entry) => {
+	  const target = entry.target;
+	  if (entry.isIntersecting) {
+		target.classList.add('show1', 'show2', 'show3');
+	  } else {
+		target.classList.remove('show1', 'show2', 'show3');
+	  }
+	});
+  });
+  
+  const hiddenElements = document.querySelectorAll('.hidden1, .hidden2, .hidden3');
+  hiddenElements.forEach((el) => observer.observe(el));
